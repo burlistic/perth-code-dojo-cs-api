b40ee23dde4d872b752fb86d71b37807dad2cba2
burlistic <will.webster@mykolab.com>
2014-10-27 22:45:30 +0800

    Managed to kill my local repo. So no commit history I am afraid

---
+++ RandomUser/Service.Integration.Tests/RandomUserServiceTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace Service.Integration.Tests
+{
+    public class RandomUserServiceTests
+    {
+        // get 1 user from the RandonUser API. A REST webservice. Less than 5 seconds
+
+        [Fact]
+        public void GetRandomUsers_Returns_Populated_User()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var randomUserService = new RandomUserService();
+
+            var users = randomUserService.GetRandomUsersAsync(false, 1);
+
+            stopWatch.Stop();
+
+            Assert.Equal(1, users.Count);
+
+            Console.Write(stopWatch.ElapsedMilliseconds);
+
+            Assert.True(stopWatch.ElapsedMilliseconds < 5000);
+        }
+
+        // get 50 users
+
+        [Fact]
+        public void GetRandomUsers_Populate_Bulk_Data_Multiple_Calls_Async()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var randomUserService = new RandomUserService();
+
+            var users = randomUserService.GetRandomUsersAsync(false, 50);
+
+            stopWatch.Stop();
+
+            Assert.Equal(50, users.Count);
+            foreach (var user in users)
+            {
+                Assert.NotNull(user.registered);
+                Assert.False(string.IsNullOrEmpty(user.name.first));
+            }
+
+            Console.Write(stopWatch.ElapsedMilliseconds);
+
+            Assert.True(stopWatch.ElapsedMilliseconds < 80000);
+        }
+
+        // how long would it take calling the service once with result=50?
+
+        [Fact]
+        public void GetRandomUsers_Populate_Bulk_Data_Single_Call_Async()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var randomUserService = new RandomUserService();
+
+            var users = randomUserService.GetRandomUsersAsync(true, 50);
+
+            stopWatch.Stop();
+
+            Assert.Equal(50, users.Count);
+            foreach (var user in users)
+            {
+                Assert.NotNull(user.registered);
+                Assert.False(string.IsNullOrEmpty(user.name.first));
+            }
+
+            Console.Write(stopWatch.ElapsedMilliseconds);
+
+            Assert.True(stopWatch.ElapsedMilliseconds < 10000);
+        }
+
+        // how long would it take without async?
+
+        // how long with a seed?
+
+        // create output - a list of users along with images
+    }
+}
---
+++ RandomUser/Service/RandomUserService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class RandomUserService
+    {
+        private readonly List<User> _users;
+
+        public RandomUserService()
+        {
+            _users = new List<User>();
+        }
+
+        public List<User> GetRandomUsersAsync(bool bulkLoad, int count)
+        {
+            if (bulkLoad)
+            {
+                GetUserAsync(true, count).Wait();
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    GetUserAsync(false, count).Wait();
+                }
+            }
+            return _users;
+        }
+
+        private async Task GetUserAsync(bool bulkLoad, int count = 0)
+        {
+            using (var client = new HttpClient())
+            {
+                
+                client.BaseAddress = new Uri("http://api.randomuser.me");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                if (bulkLoad)
+                {
+                    HttpResponseMessage response = await client.GetAsync("?results=" + count + "&key=8JE0-J3P4-MCWI-42H9");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = await response.Content.ReadAsAsync<RandomUserResponse>();
+
+                        foreach (var user in result.results)
+                        {
+                            _users.Add(user.user);
+                        }
+
+                        return;
+                    }
+                    throw new NullReferenceException("no user returned from service");
+
+
+                    // todo - handle other responces
+                }
+                else
+                {
+
+                    HttpResponseMessage response = await client.GetAsync("");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = await response.Content.ReadAsAsync<RandomUserResponse>();
+                        var firstResult = result.results.FirstOrDefault();
+                        if (firstResult != null)
+                        {
+                            Console.WriteLine("Random user, registered number: {0}", firstResult.user.registered);
+                            _users.Add(firstResult.user);
+                            return;
+                        }
+
+                        throw new NullReferenceException("no user returned from service");
+                    }
+
+                    // todo - handle other responces
+                }
+
+
+
+            }
+
+        }
+    }
+}
+    
